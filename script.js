0025e0285b41bd78498dbf9b10aab87e5c508a05
jaymoh23 <jeffreyryanmurray@gmail.com>
2017-09-26 16:24:01 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+let myName= 'Jeffrey Murray';
+
+if (myName == 'Jeffrey Murray')
+{
+    console.log("Name:" + "  " + myName .toUpperCase()); 
+}
+
+/*Career*/
+let myCareer= 'Front End Developer';
+
+{
+    console.log("\n Career:" + " " + myCareer);
+}
+
+/*Description*/
+console.log("\n Description: I like good puppers and Space X");
+
+/*Interests*/
+console.log("\n My Interests:");
+
+var Interests = [ 
+    "\ * Soccer, Basketball and Football",
+ "\n *  Music of almost all kinds",
+  "\n * Psychological Thrillers"];
+console.log(Interests);
+
+/*Previous Experience*/
+console.log("\n My Previous Experience:");
+
+
+
+
+
+
+
+
+
